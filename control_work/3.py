9e5b638f885784cf886f5a9e5a032b86e8bd04fa
romanBelyanov <romkabel412@gmail.com>
2025-04-08 09:24:46 +0700

    Update 3.py

--- control_work/3.py
+++ control_work/3.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import math
 
 class Shape(ABC):
     @abstractmethod
@@ -29,5 +30,24 @@ class Rect(Shape):
     def print_info(self):
         return f"Площадь прямоугольника равна {self.area()}\nПериметр прямоугольника равен {self.P()}"
     
-area = Rect(5, 10)
-print(area.print_info())
+
+class Circle(Shape):
+    def __init__(self, r):
+        super().__init__()
+        self.r = r
+
+    def area(self):
+        return math.pi*self.r**2
+    
+    def P(self):
+        return 2*math.pi*(self.r)
+    
+    def print_info(self):
+        return f"Площадь круга равна {self.area()}\nПериметр круга равен {self.P()}"
+    
+area = [
+    Rect(5, 10),
+    Circle(5),
+]
+for i in area:
+    print(i.print_info())
